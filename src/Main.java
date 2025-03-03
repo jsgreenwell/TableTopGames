28d4e18138c1cfbbfcae4bd807f278cb243bde63
jgreenwell <jsgreenwell83@gmail.com>
2025-03-03 11:14:50 -0600

    Added new game for learning for loops

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,120 @@ public class Main {
      * @param args Args are passed from the commandline (think flags)
      */
     public static void main(String[] args) {
-        // Display main menu & get user choice
-        System.out.println("""
-                           Welcome to our little Game System!
-                           Please select one of the following games:
-                           \t1. Even/Odd
-                           \t2. Red/Blue
-                           """);
+        // Display main menu & get user choice (repeat until there is a correct choice)
+        int choice = 0;
+        while (choice < 1 || choice > 4) {
+            clearScreen();
+            System.out.println("""
+                    Welcome to our little Game System!
+                    Please select one of the following games:
+                    \t1. Even/Odd
+                    \t2. Red/Blue
+                    \t3. Encryption Break!
+                    """);
 
-        int choice = scan.nextInt();
-        scan.nextLine(); // remove extra newline
+            choice = scan.nextInt();
+            scan.nextLine(); // remove extra newline
+        }
 
-        // TODO: Add if statement here which checks choice and plays appropriate game
-        playEvenOdd();
+        switch (choice) {
+            case 1:
+                playEvenOdd();
+                break;
+            case 2:
+                playRedBlue();
+                break;
+            case 3:
+                playWheel();
+                break;
+            default:
+
+                break;
+        }
+    }
+
+    /**
+     * The game is based on the Hangman, but I like Wheel of Fortune's name better :)
+     * A hint is given then you guess the letters of the word or guess the word.
+     */
+    private static void playWheel() {
+        printGreeting("wheel");
+        String answer = "";
+        String hint = "";
+
+        // TODO: review if/switch statements here (why is one better/worse?)
+
+        // If or Switch for get random to assign on of these options
+        /*
+        answer = "History repeats itself first as tragedy second as farce"
+        hint = "Karl Marx quote on histories cyclic nature often attributed to others."
+
+        answer = "History is written by the victors"
+        hint = "This British prime minister during WWII had many famous quotes including this one about recording events"
+
+        answer = "History is a set of lies agreed upon"
+        hint = "Before his downfall at Waterloo, this French dictator shared his cynical view of humanity's memory in this quote"
+
+        answer = "History is written by the victors but victims write the memoirs"
+        hint = "Carol Travis devoted her life to fighting psychobabble, biobunk, and pseudoscience. She famously corrected Churchill with this quote"
+        */
+
+        // See algo on board. Sprints: 1. Build basic for loops. 2. Add Strings 3. Guess Logic
+        // If time: add points
+        String guessed = "";
+        // TODO: Build for loop to build guessed here
+        // Start more code here
+
+        //TODO: Get guess and check versus string
+        // To start print the answer & hint then make sure guess works
+        // Remove answer after and just print guessed (26 guesses at max)
+        // Add difficulty select (hard = less guesses & upper/lower matter)
+
+        // Start more code here
+
+    }
+
+    /**
+     * Red vs. Blue, it's me versus you....
+     * This game is a simplified version of an old dice game.
+     * If you get red = 1 point, blue = 1 point for blue, and joker = both lose point
+     */
+    private static void playRedBlue() {
+        printGreeting("redblue");
+
+        int redTeam = 0;
+        int blueTeam = 0;
+
+        // TODO: Add a for loop to play for a number of rounds (based on asking player how many)
+        int card = getRandom(1,3);
+        switch (card) {
+            case 1:
+                System.out.println("Card is Red!");
+                redTeam++;
+                System.out.println("\tCongrats! Red Team gets a point!");
+                break;
+            case 2:
+                System.out.println("Card is Blue!");
+                blueTeam++;
+                System.out.println("\tBlue Team gets a point!");
+                break;
+            case 3:
+                System.out.println("Card is Joker!");
+                redTeam--;
+                blueTeam--;
+                System.out.println("\tJoker Steals a point from each team!");
+                break; // not needed (end of switch)
+        }
+
+        if (redTeam > blueTeam) {
+            System.out.println("You are the winner red!");
+        } else if (redTeam < blueTeam) {
+            System.out.println("Blue Team has won!");
+        } else if (redTeam == blueTeam) {
+            System.out.println("A Tie - no winners");
+        } else if (redTeam < 0 && blueTeam < 0) {
+            System.out.println("The Joker won and stole all the points!");
+        }
     }
 
     /**
@@ -30,18 +131,7 @@ public class Main {
      */
     private static void playEvenOdd() {
         // Print start page
-        clearScreen();
-        System.out.print("""
-                  ____
-                 /\\' .\\    _____
-                /: \\___\\  / .  /\\
-                \\' / . / /____/..\\
-                 \\/___/  \\'  '\\  /
-                          \\'__'\\/
-               Time to start Even & Odd (Cho-han)!
-               Press enter when your ready to continue:""");
-        scan.nextLine();
-        clearScreen();
+        printGreeting("evenodd");
 
         // Get random number for dice
         int dieOne = getRandom(1,12);
@@ -61,6 +151,52 @@ public class Main {
             System.out.printf("You have won $%.2f from a bet of $%.2f with rolls of %d & %d!\n",
                     winnings*1.0/100, bet*1.0/100, dieOne, dieTwo);
         }
+    }
+
+    /**
+     * Print out art and greeting based on passed game.
+     * @param game The game to play
+     */
+    private static void printGreeting(String game) {
+        clearScreen();
+        switch (game) {
+            case "evenodd":
+                System.out.print("""
+                  ____
+                 /\\' .\\    _____
+                /: \\___\\  / .  /\\
+                \\' / . / /____/..\\
+                 \\/___/  \\'  '\\  /
+                          \\'__'\\/
+               Time to start Even & Odd (Cho-han)!
+               Press enter when your ready to continue:""");
+                break;
+            case "redblue":
+                System.out.print("""
+                |A .  |
+                | /.\\ |
+                |(_._)|
+                |  |  |
+                |____V|
+               Time to start Red & Blue!
+               Press enter when your ready to continue:""");
+                break;
+            case "wheel":
+                System.out.print("""
+                                  _               _\s
+                                 | |             | |
+                        __      _| |__   ___  ___| |
+                        \\ \\ /\\ / / '_ \\ / _ \\/ _ \\ |
+                         \\ V  V /| | | |  __/  __/ |
+                          \\_/\\_/ |_| |_|\\___|\\___|_|
+                                                   \s
+               Time to start Wheel!
+               Press enter when your ready to continue:""");
+                break;
+        }
+        // Pause until Enter hit then clear Logo screen
+        scan.nextLine();
+        clearScreen();
     }
 
     /**
