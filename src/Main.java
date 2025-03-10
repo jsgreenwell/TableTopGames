beda11b6660279d7edfa6c10c1df62173ae2b82a
jgreenwell <jsgreenwell83@gmail.com>
2025-03-10 09:48:50 -0500

    Finished Logic for Wheel

--- src/Main.java
+++ src/Main.java
@@ -72,15 +72,11 @@ public class Main {
         // If time: add points
         String guessed = "";
 
-        for (int i = 0; i<answer.length(); i++) {
-            if (answer.charAt(i) != ' ') {
-                guessed += "_";
-            } else {
-                guessed += " ";
-            }
-        }
         /* Alternate way to build: which do you like better?
-        for (char letter : answer.toCharArray()) {
+           For Each loop that builds a string based on the letters (of ___)
+           Why var - what's var mean? (what was it before)
+         */
+        for (var letter : answer.toCharArray()) {
             if (letter != ' ') {
                 guessed += "_";
             } else {
@@ -88,32 +84,51 @@ public class Main {
             }
         }
 
-         */
-        System.out.println(guessed);
-        System.out.println(answer);
+        // Loop until solved or last letter used (i.e. run for all letters)
+        // Incorrect solve also exits but with different message
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print("Would you like to guess a letter or solve? (l/s) : ");
+            String choice = scan.nextLine().toLowerCase();
+            if (choice.startsWith("l")) {
+                // Here we start: print current guess
+                System.out.print("Current guess:\n\t");
+                System.out.println(guessed);
+                System.out.println(answer); // Remove after
 
-        for (String word : answer.split(" ")) {
-            System.out.println(word);
-        }
+                System.out.print("Enter the letter you guess: ");
+                char newguess = scan.nextLine().toLowerCase().charAt(0);
 
-        //TODO: Get guess and check versus string
-        // To start print the answer & hint then make sure guess works
-        // Remove answer after and just print guessed (26 guesses at max)
-        // Add difficulty select (hard = less guesses & upper/lower matter)
+                // flag to keep from printing the correct letter message too much
+                boolean correct = false;
 
-        System.out.print("Would you like to guess a letter or solve? (l/s) : ");
-        String choice = scan.nextLine().toLowerCase();
-        if (choice.startsWith("l")) {
-            // Here we start: for or foreach?
+                // Loop over each letter and when its correct change that same letter in guessed
+                for (int i = 0; i < answer.length(); i++) {
+                    if (answer.toLowerCase().charAt(i) == newguess) {
+                        if (!correct) {
+                            System.out.print("You guessed a correct letter!");
+                            correct = true;
+                        }
+                        guessed = guessed.substring(0, i) + answer.charAt(i) + guessed.substring(i + 1);
+                    }
+                }
+                correct = false;
 
-        } else {
-            System.out.println("Please enter your guess (no punctuation but include spaces):");
-            String finalGuess = scan.nextLine();
-
-            if (finalGuess.equalsIgnoreCase(answer)) {
-                System.out.println("You guessed correctly!");
+                System.out.println("Correct guesses: " + guessed);
             } else {
-                System.out.println("Your guess is not correct!");
+                System.out.println("Guessed Phrase letters: \n\t" + guessed);
+                System.out.println("Please enter your guess (no punctuation but include spaces):");
+                String finalGuess = scan.nextLine();
+
+                if (finalGuess.equalsIgnoreCase(answer)) {
+                    System.out.println("You guessed correctly!");
+                } else {
+                    System.out.println("Your guess is not correct!");
+                }
+
+                // Right or wrong its time to exit
+                System.out.print("Thanks for playing! Hit any enter to exit...");
+                scan.nextLine();
+                return;
             }
         }
 
